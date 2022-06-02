ab7c7c17544d385e9174a12668af4bef042703f2
Aironys Garrido <aironys1@hotmail.com>
2022-06-02 02:01:59 -0300

    Comentários JS

---
+++ comentario.js
@@ -0,0 +1,13 @@
+// Comentário em linha
+console.log("Vamos falar de comentário");
+
+console.log("Comentário em linha"); // Comentário em linha
+
+
+/*
+* Comentário em bloco
+* Comentário em bloco
+* Comentário em bloco
+* Comentário em bloco
+*/
+
