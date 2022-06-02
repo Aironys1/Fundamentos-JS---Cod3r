a9e86daacaf772922e129ece1d05998775235fc2
Aironys Garrido <aironys1@hotmail.com>
2022-06-02 01:58:28 -0300

    Bloco JS

---
+++ bloco.js
@@ -0,0 +1,49 @@
+{
+    console.log("Primeiro bloco de código")
+}
+
+{
+    console.log("Segundo bloco de código")
+    console.log("Segundo bloco de código")
+    console.log("Segundo bloco de código")
+
+}
+
+{
+    {
+        console.log("Bloco dentro de bloco")
+
+    }
+}
+
+{
+    ;  // Blco de código vazio
+    ;   // Blco de código vazio
+    ;   // Blco de código vazio
+}
+
+{
+    {
+
+        {
+            {
+                console.log("4 blocos de códigos");
+            }
+        }
+
+    }
+}
+
+
+/*
+ORGANIZAÇÃO 
+
+PASTAS 
+
+ARQUIVOS 
+
+SENTENÇA DE CÓDIGOS 
+
+BLOCOS DE CÓDIGOS
+
+*/
